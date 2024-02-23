4d9d8a41d5833366f80128055f3424b49f54fca3
courseKA <150334193+courseKA@users.noreply.github.com>
2024-02-23 05:35:43 +0200

    Update jarcomp_scrapy.py

--- jarcomp_scrapy.py
+++ jarcomp_scrapy.py
@@ -1,1 +1,14 @@
+import scrapy
 
+class JarcomputersSpider(scrapy.Spider):
+  name = "Jarcomputers"
+  urls = urls = [
+    "https://www.jarcomputers.com/Laptopi_cat_2.html?ref=c_1",
+    "https://www.jarcomputers.com/laptopi-cat-2.html?ref=c_1&page=2",
+    "https://www.jarcomputers.com/laptopi-cat-2.html?ref=c_1&page=3",
+    "https://www.jarcomputers.com/laptopi-cat-2.html?ref=c_1&page=4",
+    "https://www.jarcomputers.com/laptopi-cat-2.html?ref=c_1&page=5",
+]
+
+def parse(self.response):
+  
